dc7a4447c9a7ae5041a183d7de7c52cbaf1f2f8a
unknown <andriescuioana@gmail.com>
2020-05-14 19:35:10 +0200

    Updates

---
+++ precache-manifest.86480b55d5f6e269734f7fcfb3665ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63c149b07ab54ae5860d5ee4256b0c0",
+    "url": "/markdown-editor/index.html"
+  },
+  {
+    "revision": "c49f0427e28a7116cbde",
+    "url": "/markdown-editor/static/css/main.7fb78e6f.chunk.css"
+  },
+  {
+    "revision": "b76e90ab0f8616d04a94",
+    "url": "/markdown-editor/static/js/2.42b80de7.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/markdown-editor/static/js/2.42b80de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49f0427e28a7116cbde",
+    "url": "/markdown-editor/static/js/main.928106ab.chunk.js"
+  },
+  {
+    "revision": "400c89452c10e569cab7",
+    "url": "/markdown-editor/static/js/runtime-main.056b8f29.js"
+  }
+]);
