262db32036825d8f3e5780d3f47ab59028eaa34c
unknown <andriescuioana@gmail.com>
2020-05-14 17:01:54 +0200

    add React Markdown to parse HTML

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 function App() {
   const [markdown, setMarkdown] = useState('# Hello World');
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  };
+
   return (
     <div className="app">
-      <textarea value={markdown} />
-      <div className="preview">Hello World</div>
+      <textarea onChange={handleChange} value={markdown} />
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
